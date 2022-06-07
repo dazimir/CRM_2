8392472e6feb2f692e19fb1ba701817d90fd7e8a
ramil <dazimir@yandex.ru>
2022-06-08 01:57:55 +0300

    подготовил шаблон сайта CRM добавил страницы, далее приступим к заполнению

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from apps.core import views as core_views
 
 
 urlpatterns = [
-    path('', core_views.main, name='main-url')
+    path('admin', core_views.admin , name='admin'),
+    path('', core_views.index, name='index'),
+    path('reception', core_views.reception, name='reception'),
+    path('applicant', core_views.applicant, name='applicant'),
+    path('cards', core_views.cards, name='cards'),
+    path('task_status', core_views.task_status, name='task_status'),
+    path('executor', core_views.executor, name='executor'),
+    path('report', core_views.report, name='report'),
+    path('waiting', core_views.waiting, name='waiting'),
+    path('connection', core_views.connection, name='connection')
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,9 +1,51 @@
 from django.http import HttpRequest, JsonResponse
+from django.shortcuts import render
 
 
-def main(request: HttpRequest) -> JsonResponse:
-    return JsonResponse(data={
-        "error": False,
-        "status": "Hello from Ramil Django",
-        "details": None
-    }, status=200)
+# def main(request: HttpRequest) -> JsonResponse:
+#     return JsonResponse(data={
+#         "error": False,
+#         "status": "Hello from Ramil Django",
+#         "details": None
+#     }, status=200)
+
+
+def admin(request):
+    return render(request, 'admin')
+
+
+def index(request):
+    return render(request, 'core/index.html')
+
+
+def reception(request):
+    return render(request, 'core/reception.html')
+
+
+def applicant(request):
+    return render(request, 'core/applicant.html')
+
+
+def cards(request):
+    return render(request, 'core/cards.html')
+
+
+def task_status(request):
+    return render(request, 'core/task_status.html')
+
+
+def executor(request):
+    return render(request, 'core/executor.html')
+
+
+def report(request):
+    return render(request, 'core/report.html')
+
+
+def waiting(request):
+    return render(request, 'core/waiting.html')
+
+
+def connection(request):
+    return render(request, 'core/connection.html')
+
