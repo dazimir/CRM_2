1840bffe6007c8fb78c8240c660f52715bc04a53
ramil <dazimir@yandex.ru>
2022-07-01 14:15:00 +0300

    инфо об операторах в бд и из бд

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,23 +1,13 @@
 from django.contrib import admin
 
-from apps.core.models import Task   #Research, Customer, Order, Organization, Statement
+from apps.core.models import Task, Authentication
 
-# @admin.register(Research)
-# class ResearchAdmin(admin.ModelAdmin):
-#     ...
+
+@admin.register(Authentication)
+class CustomerAdmin(admin.ModelAdmin):
+    ...
+
 
 @admin.register(Task)
 class CustomerAdmin(admin.ModelAdmin):
     ...
-#
-# @admin.register(Order)
-# class OrderAdmin(admin.ModelAdmin):
-#     ...
-#
-# @admin.register(Organization)
-# class OrganizationAdmin(admin.ModelAdmin):
-#     ...
-#
-# @admin.register(Statement)
-# class StatementAdmin(admin.ModelAdmin):
-#     ...
--- apps/core/models.py
+++ apps/core/models.py
@@ -2,6 +2,19 @@ from django.db import models
 from django.db.models import QuerySet
 
 
+class Authentication(models.Model):
+    user_nickname = models.CharField('Никнейм', max_length=50)
+    user_name = models.CharField('Имя оператора', max_length=50)
+    user_lastname = models.CharField('Фамилие оператора', max_length=50)
+    user_patronymic = models.CharField('Отчество оператора', max_length=50)
+    user_birthday = models.DateField('Дата рождения')
+    user_registration = models.DateField('Дата регистрации пользователя')
+    user_phone = models.CharField('Телефон оператора', max_length=12)
+    user_email = models.EmailField()
+    user_photo = models.ImageField(upload_to='static', height_field=None, width_field=None, max_length=100)
+    user_rang = models.IntegerField()
+
+
 class Task(models.Model):
     title = models.CharField('Название задачи', max_length=50)
     task = models.TextField('описание')
